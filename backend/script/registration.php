9d3181d21aae9531785ddddeb1076a4320bd67be
Rico <andreavalentino.ricotti@gmail.com>
2024-02-20 23:16:23 +0100

    Aggiunta di un po' di http_response_code commentati

--- backend/script/registration.php
+++ backend/script/registration.php
@@ -9,31 +9,37 @@
             //aggiungo campi per la registrazione avvenuta con successo
             $result['result'] = 'OK';
             $result['message'] = 'Registrazione avvenuta con successo';
+            //http_response_code(200);
             break;
         case registerResult::EMAIL_ALREADY_EXISTS:
             //aggiungo campi per l'esito negativo della registrazione
             $result['result'] = 'KO';
             $result['message'] = 'EMAIL_ALREADY_EXISTS';
+            //http_response_code(400);
             break;
         case registerResult::DB_ERROR:
             //aggiungo campi per l'esito negativo della registrazione
             $result['result'] = 'KO';
             $result['message'] = 'DB_ERROR';
+            //http_response_code(500);
             break;    
         case registerResult::MISSING_FIELDS:
             //aggiungo campi per l'esito negativo della registrazione
             $result['result'] = 'KO';
             $result['message'] = 'MISSING_FIELDS';
+            //http_response_code(400);
             break;
         case registerResult::WRONG_EMAIL_FORMAT:
             //aggiungo campi per l'esito negativo della registrazione
             $result['result'] = 'KO';
             $result['message'] = 'WRONG_EMAIL_FORMAT';
+            //http_response_code(400);
             break;
         case registerResult::DIFFERENT_PASSWORDS:
             //aggiungo campi per l'esito negativo della registrazione
             $result['result'] = 'KO';
             $result['message'] = 'DIFFERENT_PASSWORDS';
+            //http_response_code(400);
             break;
     }
     
--- backend/script/update_profile.php
+++ backend/script/update_profile.php
@@ -21,30 +21,37 @@ switch($tmp){
     case updateResult::DB_ERROR:
         $result['result'] = 'KO';
         $result['message'] = 'DB_ERROR';
+        //http_response_code(500);
         break;
     case updateResult::ERROR_UPDATE:
         $result['result'] = 'KO';
         $result['message'] = 'ERROR_UPDATE';
+        //http_response_code(500);
         break;
     case updateResult::MISSING_FIELDS:
         $result['result'] = 'KO';
         $result['message'] = 'ERROR_MISSINGFIELDS';
+        //http_response_code(400);
         break;
     case updateResult::WRONG_EMAIL_FORMAT: 
         $result['result'] = 'KO';
         $result['message'] = 'ERROR_WRONGEMAILFORMAT';
+        //http_response_code(400);
         break;
     case updateResult::WRONG_IMAGE_FORMAT: 
         $result['result'] = 'KO';
         $result['message'] = 'ERROR_WRONGIMAGEFORMAT';
+        //http_response_code(400);
         break;
     case updateResult::DUPLICATE_EMAIL:
         $result['result'] = 'KO';
         $result['message'] = 'DUPLICATE_EMAIL';
+        //http_response_code(400);
         break;
     case updateResult::SUCCESSFUL_UPDATE:
         $result['result'] = 'OK';
         $result['message'] = 'SUCCESSFUL_UPDATE';
+        //http_response_code(200);
         break;
 }
 echo json_encode($result);
