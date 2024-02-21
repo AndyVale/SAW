448279145a1d4ff57642f539f70bb8035287abc3
AndyVale <81222307+AndyVale@users.noreply.github.com>
2024-02-21 01:11:06 +0100

    Names to lowercase

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@
 -- Salvati(idCartella, idPost)
 
 -- Implementazione SQL:
-create table Utente(
+create table utente(
     ID int not null auto_increment,
     email varchar(255) not null unique,
     firstname varchar(255) not null,
@@ -20,7 +20,7 @@ create table Utente(
     primary key(ID)
 );
 
-create table Post(
+create table post(
     ID int not null auto_increment,
     idUtente int not null,
     oraPubblicazione timestamp not null DEFAULT CURRENT_TIMESTAMP,
@@ -30,7 +30,7 @@ create table Post(
     foreign key(idUtente) references Utente(ID) ON DELETE CASCADE
 );
 
-create table Liked(
+create table liked(
     idUtente int not null,
     idPost int not null,
     primary key(idUtente, idPost),
@@ -38,7 +38,7 @@ create table Liked(
     foreign key(idPost) references Post(ID) ON DELETE CASCADE
 );
 
-create table Seguiti(
+create table seguiti(
     idUtente int not null,
     idUtenteSeguito int not null,
     primary key(idUtente, idUtenteSeguito),
@@ -47,14 +47,14 @@ create table Seguiti(
     CONSTRAINT no_autofollow CHECK (idUtente != idUtenteSeguito)
 );
 
-create table CartellaSalvati(
+create table cartellaSalvati(
     ID int not null auto_increment,
     idUtente int not null,
     primary key(ID),
     foreign key(idUtente) references Utente(ID) ON DELETE CASCADE
 );
 
-create table Salvati(
+create table salvati(
     idCartella int not null,
     idPost int not null,
     primary key(idCartella, idPost),
